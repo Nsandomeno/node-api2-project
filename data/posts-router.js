678ff7838748d451ec0ec536c6961e4c7e468057
Nicholas Sandomeno <nicholassandomeno@Nicholass-Air.home>
2020-02-11 19:15:28 -0500

    5 endpoints complete.

--- data/posts-router.js
+++ data/posts-router.js
@@ -72,5 +72,39 @@ router.get("/", (req,res) => {
     })
 })
 
+// (4) - get - returns individual post object by id
+router.get("/:id", (req, res) => {
+    const { id } = req.params
+    
+    Posts.findById(id).then((shape) => {
+        if (shape.length === 0) {
+            res.status(404).json({ message: "A post with this id could not be found." })
+        } else {
+            res.status(200).json(shape)
+        }
+    }).catch((error) => {
+        res.status(500).json({ message:"The post information could not be retrieved." })
+    })
+})
+
+// (5) - get - returns comments for a given post
+router.get("/:id/comments", (req, res) => {
+    const { id } = req.params
+
+    Posts.findById(id).then((shape) =>{
+        if (shape.length === 0) {
+            res.status(404).json({ message: "No post with this id could be found." })
+        } else {
+            Posts.findPostComments(id).then((comments) => {
+                res.status(200).json(comments)
+            }).catch((error) => {
+                res.status(500).json({ message: "The Post info could not be retrieved." })
+            })
+        }
+    }).catch((error) => {
+        res.status(500).json({ message: "The post info could not be retrived." })
+    })
+})
+
 // export
 module.exports = router
